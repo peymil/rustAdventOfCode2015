3806fd1d681f98ddf8afd421aa07d6ac61e5f3b6
Emre Badem <emrebadem_@hotmail.com>
2022-03-13 05:27:06 +0300

    Remove unecessary src folder.

--- src/main.rs
+++
@@ -1,9 +0,0 @@
-// "(" one floor up
-// ")" one floor down
-use std::env;
-use std::fs;
-fn main() {
-    let contents = fs::read_to_string("./src/input.txt")
-    .expect("Something went wrong reading the file");
-    contents.chars().into_iter().map(|x| println!("{}",x));
-}
